594de142aee907fbf9e8ccb9b8ba3206b93b0135
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-10-22 07:01:40 +0530

    initial project setup

---
+++ app-petclinic/src/main/java/app/petclinic/AppPetclinicApplication.java
@@ -0,0 +1,13 @@
+package app.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPetclinicApplication.class, args);
+	}
+
+}
