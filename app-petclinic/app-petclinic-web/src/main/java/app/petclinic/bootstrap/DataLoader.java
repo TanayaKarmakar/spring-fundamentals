d36529fb08a137d9c6b29327052bb76c95edd46c
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-10-26 19:33:19 +0530

    Refactored Map impl to use auto generated ID, Closes #23

--- app-petclinic/app-petclinic-web/src/main/java/app/petclinic/bootstrap/DataLoader.java
+++ app-petclinic/app-petclinic-web/src/main/java/app/petclinic/bootstrap/DataLoader.java
@@ -28,7 +28,6 @@ public class DataLoader implements CommandLineRunner {
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Glennane");
 		
@@ -37,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Loaded Owners .... ");
 		
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Sam");
 		vet1.setLastName("Axe");
 		
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Jessie");
 		vet2.setLastName("Porter");
 		
