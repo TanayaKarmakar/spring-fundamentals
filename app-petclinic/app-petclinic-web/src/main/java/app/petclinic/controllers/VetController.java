172d730eb8fe05d35e53af9a5cdc8be9eeae2661
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-11-20 06:06:17 +0530

    code

--- app-petclinic/app-petclinic-web/src/main/java/app/petclinic/controllers/VetController.java
+++ app-petclinic/app-petclinic-web/src/main/java/app/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package app.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import app.petclinic.model.Vet;
 import app.petclinic.services.VetService;
 
 @Controller
@@ -14,9 +19,14 @@ public class VetController {
 		this.vetService = vetService;
 	}
 	
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+	@GetMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
 	public String listVets(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		return vetService.findAll();	
 	}
 }
