db32b53d07fce33922f7b7b3e5be63b638d25742
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-12-06 18:33:49 +0530

    deleted

--- spring-boot-recipe-mongo/src/main/java/com/app/recipe/repositories/reactive/RecipeReactiveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.recipe.repositories.reactive;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.app.recipe.domain.Recipe;
-
-public interface RecipeReactiveRepository extends ReactiveMongoRepository<Recipe, String> {
-
-}
--- spring-boot-recipe-mongo/src/main/java/com/app/recipe/repositories/reactive/UnitOfMeasureReactiveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.recipe.repositories.reactive;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.app.recipe.domain.UnitOfMeasure;
-
-public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
-
-}
