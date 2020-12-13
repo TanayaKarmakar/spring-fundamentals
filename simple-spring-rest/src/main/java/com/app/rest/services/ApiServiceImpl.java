3090c49b01ef26654a0d103c4dd5d7deeadd8888
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-12-13 06:56:10 +0530

    code

--- simple-spring-rest/src/main/java/com/app/rest/services/ApiServiceImpl.java
+++ simple-spring-rest/src/main/java/com/app/rest/services/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package com.app.rest.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.app.rest.api.domain.User;
 import com.app.rest.api.domain.UserData;
@@ -11,14 +13,20 @@ import com.app.rest.api.domain.UserData;
 @Service
 public class ApiServiceImpl implements ApiService {
 	private final RestTemplate restTemplate;
+	private final String api_url;
 	
-	public ApiServiceImpl(RestTemplate restTemplate) {
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
 		this.restTemplate = restTemplate;
+		this.api_url = api_url;
 	}
 
 	@Override
 	public List<User> getUsers(Integer limit) {
-		UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit,
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("limit", limit);
+		
+		UserData userData = restTemplate.getForObject(uriBuilder.toUriString(),
 				UserData.class);
 		return userData.getData();
 	}
